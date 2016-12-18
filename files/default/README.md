7dd019170ff0321ff0adfc7b2b43a80cae671d23
Mate Lakat <mlakat@suse.com>
2016-12-18 16:45:03 +0100

    Unit testing neutron-ha-tool.py
    Adding some unit tests that cover neutron-ha-tool's areas where target
    agents are selected.

---
+++ files/default/README.md
@@ -0,0 +1,13 @@
+# Unit Testing
+
+Make a python 2 virtual environment, activate it, and install the requirements:
+
+    pip install -r test-requirements.txt
+
+Run the tests
+
+    coverage run test-neutron-ha-tool.py
+
+Coverage report
+
+    coverage report -i neutron-ha-tool.py
---
+++ files/default/test-neutron-ha-tool.py
@@ -0,0 +1,149 @@
+import unittest
+import collections
+import importlib
+import logging
+ha_tool = importlib.import_module("neutron-ha-tool")
+
+
+class MockNeutronClient(object):
+
+    def __init__(self):
+        self.routers = {}
+        self.agents = {}
+        self.routers_by_agent = collections.defaultdict(set)
+
+    def tst_add_agent(self, agent_id, props):
+        self.agents[agent_id] = dict(props, id=agent_id)
+
+    def tst_add_router(self, agent_id, router_id, props):
+        self.routers[router_id] = dict(props, id=router_id)
+        self.routers_by_agent[agent_id].add(router_id)
+
+    def tst_agent_by_router(self, router_id):
+        for agent_id, router_ids in self.routers_by_agent.items():
+            if router_id in router_ids:
+                return self.agents[agent_id]
+
+        raise NotImplementedError()
+
+    def list_agents(self):
+        return {
+            'agents': self.agents.values()
+        }
+
+    def list_routers_on_l3_agent(self, agent_id):
+        return {
+            'routers': [
+                self.routers[router_id]
+                for router_id in self.routers_by_agent[agent_id]
+            ]
+        }
+
+    def remove_router_from_l3_agent(self, agent_id, router_id):
+        self.routers_by_agent[agent_id].remove(router_id)
+
+    def add_router_to_l3_agent(self, agent_id, router_body):
+        self.routers_by_agent[agent_id].add(router_body['router_id'])
+
+    def list_ports(self, device_id, fields):
+        return {
+            'ports': [
+                {
+                    'id': 'someid',
+                    'binding:host_id':
+                        self.tst_agent_by_router(device_id)['host'],
+                    'binding:vif_type': 'non distributed',
+                    'status': 'ACTIVE'
+                }
+            ]
+        }
+
+    def list_floatingips(self, router_id):
+        return {
+            'floatingips': [
+                {
+                    'id': 'irrelevant',
+                    'status': 'ACTIVE'
+                }
+            ]
+        }
+
+
+def make_neturon_client(live_agents=0, dead_agents=0):
+    neutron_client = MockNeutronClient()
+
+    for i in range(live_agents):
+        neutron_client.tst_add_agent(
+            'live-agent-{}'.format(i), {
+                'agent_type': 'L3 agent',
+                'alive': True,
+                'admin_state_up': True,
+                'host': 'live-agent-{}-host'.format(i),
+                'configurations': {
+                    'agent_mode': 'Mode X'
+                }
+            }
+        )
+    for i in range(dead_agents):
+        neutron_client.tst_add_agent(
+            'dead-agent-{}'.format(i), {
+                'agent_type': 'L3 agent',
+                'alive': False,
+                'admin_state_up': False,
+                'host': 'dead-agent-{}-host'.format(i)
+            }
+        )
+    return neutron_client
+
+
+class TestL3AgentMigrate(unittest.TestCase):
+
+    def test_no_dead_agents_returns_zero(self):
+        neutron_client = make_neturon_client(live_agents=2)
+
+        result = ha_tool.l3_agent_migrate(neutron_client)
+
+        self.assertEqual(0, result)
+
+    def test_no_alive_agents_returns_one(self):
+        neutron_client = make_neturon_client(dead_agents=2)
+
+        result = ha_tool.l3_agent_migrate(neutron_client)
+
+        self.assertEqual(1, result)
+
+    def test_router_moved(self):
+        neutron_client = make_neturon_client(live_agents=1, dead_agents=1)
+        neutron_client.tst_add_router('dead-agent-0', 'router-1', {})
+
+        result = ha_tool.l3_agent_migrate(neutron_client, now=True)
+
+        self.assertEqual(0, result)
+        self.assertEqual(
+            set(['router-1']), neutron_client.routers_by_agent['live-agent-0'])
+
+
+class TestL3AgentEvacuate(unittest.TestCase):
+
+    def test_no_agents_returns_zero(self):
+        neutron_client = MockNeutronClient()
+        result = ha_tool.l3_agent_evacuate(neutron_client, 'host1')
+
+        self.assertEqual(0, result)
+
+    def test_evacuation(self):
+        neutron_client = make_neturon_client(live_agents=2)
+        neutron_client.tst_add_router('live-agent-0', 'router', {})
+
+        result = ha_tool.l3_agent_evacuate(neutron_client, 'live-agent-0-host')
+
+        self.assertEqual(0, result)
+        self.assertEqual(
+            set(['router']),
+            neutron_client.routers_by_agent['live-agent-1']
+        )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
